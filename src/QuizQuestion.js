0d7422aafcf64d73deb464379ad55b0f6d5c78a5
PetNil <peter.work@live.se>
2020-09-16 05:45:06 +0200

    Update

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,7 +10,8 @@ class QuizQuestion extends Component {
                     <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
-                  <ul>
+                    <ul>
+                        <QuizEnd />
                         <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
                   </ul>
                 </section>
