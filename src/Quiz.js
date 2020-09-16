825bea0e06ac8d69385f1d76bacbcf5a6480b74d
PetNil <peter.work@live.se>
2020-09-16 17:39:10 +0200

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,7 +15,11 @@ class Quiz extends Component {
             return { quiz_position: state.quiz_position + 1 }
         })
     }
-        handleResetClick() { }
+    handleResetClick() {
+        this.setState((state) => {
+            return { quiz_position: 1 }
+        })
+    }
     
     render() {
         const isQuizEnd = ( (this.state.quiz_position - 1) === quizData.quiz_questions.length );
